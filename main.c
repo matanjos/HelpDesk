73afa61a0bf44beb81b934cc5b5731dd69dc5beb
Jonas Santos <jonas.cassiano@hotmail.com>
2017-10-03 23:53:06 -0300

    att 03 10 com printfs

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h> 
+#include<math.h>
 
 typedef struct net{
     unsigned char dig1;
@@ -34,11 +35,234 @@ ARV_BIN_ENC left(ARV_BIN_ENC);
 ARV_BIN_ENC right(ARV_BIN_ENC);
 ARV_BIN_ENC brother(ARV_BIN_ENC);
 ARV_BIN_ENC pesqID(ARV_BIN_ENC, int);
-
+ARV_BIN_ENC remocaoPorCopia(ARV_BIN_ENC,int);
 
 void cabecalho();
 void fpause2();
 MAQUINA lerinfos(MAQUINA);
+
+int conta_nodoright(ARV_BIN_ENC arvaux){
+    int aux ;
+    for(aux = 0;arvaux; ++aux, arvaux=arvaux->right);
+    return aux;
+}
+int conta_nodoleft(ARV_BIN_ENC arvaux){
+    int aux ;
+    for(aux = 0;arvaux; ++aux, arvaux=arvaux->left);
+    return aux;
+}
+
+ARV_BIN_ENC rotacao_direita(ARV_BIN_ENC arvore){
+    ARV_BIN_ENC aux1, aux2;
+    aux1 = (arvore)->left;        
+    aux2 = aux1->right;
+    (arvore)->left = aux2;
+    aux1->right = (arvore);
+    arvore = aux1;
+    return(arvore);
+}
+
+ARV_BIN_ENC rotacao_esquerda(ARV_BIN_ENC arvore){
+    ARV_BIN_ENC aux1, aux2;
+    aux1 = (arvore)->right;        
+    aux2 = aux1->left;
+    (arvore)->right = aux2;
+    aux1->left = (arvore);
+    arvore = aux1;
+    return(arvore);
+}
+
+ARV_BIN_ENC EspinhaDorsal(ARV_BIN_ENC arvore){
+    ARV_BIN_ENC tmp,father, ant;
+    int aux = 1;
+    father = tmp = ant = arvore;
+    while(tmp){
+        printf("tmp atual %d!\n",(tmp->pc).id);
+        if(tmp->left){
+            tmp = rotacao_direita(tmp);
+            if(aux)
+                father = tmp;   //GAMBIARRA
+            else
+                ant->right = tmp;
+            printf("tmp = rotacao direita %d!\n",(tmp->pc).id);
+        }else{
+            aux = 0;
+            ant = tmp;
+            tmp = tmp->right;
+            if(tmp)
+                printf("tmp = right%d!\n",(tmp->pc).id);
+        }
+    }
+    printf("father %d!\n",(father->pc).id);
+    return father;
+}
+ARV_BIN_ENC balancearDorsal(ARV_BIN_ENC arvore){
+    int n = conta_nodoright(arvore), m, i;
+    ARV_BIN_ENC aux = arvore,aux2;
+    m = (pow(2,floor(log2(n+1))))-1;
+    printf("m = %d , n = %d \n",m,n);
+    for(i=0 ; i<(n-m) ; i++){
+        if(!i){
+            printf("aux antes rotacao esq %d!\n",(aux->pc).id);            
+            aux = rotacao_esquerda(arvore);
+            arvore = aux;
+            aux2= arvore;
+            printf("new arv = %d!\n",(arvore->pc).id); 
+        }else{           
+            aux = rotacao_esquerda(aux->right);
+            printf("new aux %d!\n",(aux->pc).id); 
+            printf("aux2 %d!\n",(aux2->pc).id);             
+            aux2->right = aux;
+            aux2 = aux2->right;
+            
+            printf("new aux2 %d!\n",(aux2->pc).id);    
+           
+            if(aux->right)
+                printf("aux right %d!\n",(aux->right->pc).id);            
+            
+        }
+            printf("\naux atual %d!\n",(aux->pc).id); 
+            if (aux->right)
+                printf("aux atual right %d!\n",(aux->right->pc).id);   
+            if (aux->left) 
+                printf("aux atual left %d!\n",(aux->left->pc).id);   
+            printf("arvore = %d!\n\n",(arvore->pc).id); 
+            
+    }
+            printf("primeiro for passou! nodo direita : %d\n",conta_nodoright(arvore));            
+            
+    while(m>1){
+        m = m/2;
+        printf("m %d!\n",m);   
+        printf("aux atual %d!\n",(aux->pc).id);
+        aux = rotacao_esquerda(arvore);
+        printf("new aux rot%d!\n",(aux->pc).id);
+        arvore = aux2 = aux;
+        if (aux->right)
+                printf("aux atual right %d!\n",(aux->right->pc).id);   
+        if (aux->left) 
+                printf("aux atual left %d!\n",(aux->left->pc).id); 
+        for(i=0;i< m-1;i++){
+            printf("aux atual %d!\n",(aux->pc).id);
+            if (aux->right)
+                printf("aux atual right %d!\n",(aux->right->pc).id);   
+            if (aux->left) 
+                printf("aux atual left %d!\n",(aux->left->pc).id);  
+            aux = rotacao_esquerda(aux->right);
+            printf("new aux rot%d!\n",(aux->pc).id); 
+            printf("aux2 %d!\n",(aux2->pc).id);                
+            aux2->right = aux;
+            aux2 = aux2->right;
+            printf("new aux2 %d!\n",(aux2->pc).id);                
+        }
+    }
+    return aux;
+}
+
+/*createPerfectTree (root, n) {
+m = (2 elevado ao menor valor de lg(n+1)) -
+1;
+faça n –
+m rotações começando da raiz;
+while (m > 1) {
+m = m / 2;
+faça m rotações começando da raiz;
+}
+}
+*/
+ARV_BIN_ENC ALGORITMODSW(ARV_BIN_ENC arvore){
+    if(arvore)
+        return balancearDorsal(EspinhaDorsal(arvore)); 
+    return NULL;
+}
+
+
+//--------------------------------------------------------------------------------------------------------
+int main(){
+    int menu,aux;
+    ARV_BIN_ENC info = NULL,arvaux = NULL;
+    MAQUINA computador;
+    do{
+        cabecalho();
+        printf("1 - CADASTRAR COMPUTADOR\n");
+        printf("2 - LISTAR EM ORDEM CRESCENTE (PERCURSO IN-ORDEM): \n");
+        printf("3 - REMOVER: \n");
+        printf("4 - : \n");
+        printf("5 - BALANCEAR: \n");
+        printf("6 - PESQUISA POR ID: \n");
+        printf("7 - FILHOS A DIREITA (ESPINHA DORSAL): \n");
+        printf("8 - SAIR: \n");
+        scanf("%d",&menu);
+
+        switch(menu){
+            case 1:
+            		cabecalho();
+                    info = ins_ele(info,lerinfos(computador));
+                    fpause2();
+                break;
+            case 2:
+			        InOrdem(info);
+                    fpause2();
+                break;
+            case 3:
+                    printf("Informe o ID a ser removido: \n");
+                    scanf("%d",&aux);
+                    if(pesqID(info,aux)){
+                        info = remocaoPorCopia(info, aux);
+                        printf("Removido com sucesso!\n");
+                    }else{
+                        printf("ID nao encontrado na base de dados!\n");
+                    }
+                    fpause2();
+                break;
+            case 4:
+                    
+                   
+                break;
+            case 5:
+                if(info){
+                    info = ALGORITMODSW(info);
+                    printf("Base otimizada com sucesso!\n");
+                }else{
+                    printf("base de dados vazia!\n");
+                }
+                fpause2();
+                break;
+            case 6:
+                printf("Informe o ID a ser procurado: \n");
+                scanf("%d",&aux);
+                arvaux = pesqID(info, aux);
+                if(!arvaux){
+                    printf("ID nao encontrado! \n");
+                }else{
+                    listarNo(arvaux);
+                }
+                fpause2();   
+                break;
+            case 7:
+                arvaux = info;
+                printf("contador nivel filho direita %d e esq: %d\n",conta_nodoright(arvaux), conta_nodoleft(arvaux));
+                fpause2();
+                break;
+            case 8:
+                printf("Sistema finalizado. \n");
+		        fpause2();               
+              break;
+            default: 
+                printf("Opcao invalida\n");
+               fpause2();   
+        }
+    }while(menu != 8);
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+//void buscaIDprint(ARV_BIN_ENC, int);
+
+//void balancearArv(ARV_BIN_ENC *);
+
 
 ARV_BIN_ENC remocaoPorCopia(ARV_BIN_ENC arvore,int id) {
     if(!arvore)
@@ -69,120 +293,10 @@ ARV_BIN_ENC remocaoPorCopia(ARV_BIN_ENC arvore,int id) {
                 father->left = tmp->left;
             else
                 father->right = tmp->left;
+            free (tmp);
     }
         return arvore;
 }
-
-
-//--------------------------------------------------------------------------------------------------------
-int main(){
-    int menu,aux;
-    ARV_BIN_ENC info = NULL,arvaux = NULL;
-    MAQUINA computador;
-    do{
-        cabecalho();
-        printf("1 - CADASTRAR COMPUTADOR\n");
-        printf("2 - LISTAR EM ORDEM CRESCENTE (PERCURSO IN-ORDEM): \n");
-        printf("3 - REMOVER: \n");
-        printf("5 - BALANCEAR: \n");
-        printf("6 - PESQUISA POR ID: \n");
-        printf("7 - PESQUISA POR ID LISTANDO VISITAS: \n");
-        printf("8 - SAIR: \n");
-        scanf("%d",&menu);
-
-        switch(menu){
-            case 1:
-            		cabecalho();
-                    info = ins_ele(info,lerinfos(computador));
-                    fpause2();
-                break;
-            case 2:
-			        InOrdem(info);
-                    fpause2();
-                break;
-            case 3:
-            
-                    printf("InOrdem traversal of the given tree \n");
-                    InOrdem(info);
-                
-                    printf("\nDelete 12\n");
-                    info = remocaoPorCopia(info, 12);
-                    printf("InOrdem traversal of the modified tree \n");
-                    InOrdem(info);
-                
-                    printf("\nDelete 30\n");
-                    info = remocaoPorCopia(info, 30);
-                    printf("InOrdem traversal of the modified tree \n");
-                    InOrdem(info);
-                
-                    printf("\nDelete 14\n");
-                    info = remocaoPorCopia(info, 14);
-                    printf("InOrdem traversal of the modified tree \n");
-                    InOrdem(info);
-                    fpause2();
-                break;
-            case 4:
-                    
-                break;
-            case 5:
-                //BALANCEAR
-                break;
-            case 6:
-                    printf("Informe o ID a ser procurado: \n");
-                    scanf("%d",&aux);
-                    arvaux = pesqID(info, aux);
-                    if(!arvaux){
-                        printf("ID nao encontrado! \n");
-                    }else{
-                        listarNo(arvaux);
-                    }
-                    fpause2();   
-                break;
-            case 7:
-                //PESQUISA ID LISTANDO
-                break;
-            case 8:
-                printf("Sistema finalizado. \n");
-		        fpause2();               
-              break;
-            default: 
-                printf("Opcao invalida\n");
-               fpause2();   
-        }
-    }while(menu != 8);
-
-    return 0;
-}
-
-//--------------------------------------------------------------------------------------------------------
-
-/*ARV_BIN_ENC ins_ele(ARV_BIN_ENC *arv, MAQUINA comp){ //recursiva, ubuntu
-    if (!(*arv)){
-        maketree(arv,comp);
-        printf("entrou");
-    }
-    else
-        if ((comp.id) < ((*arv)->pc).id)
-            (*arv)->left = ins_ele(&((*arv)->left), comp);
-        else
-            (*arv)->right = ins_ele(&((*arv)->right), comp);
-    return(*arv);
-
-
-
-    //------------------///
-     printf("paiinfo: %d\n",(info->pc).id);
-                   printf("filho esq info: %d\n",((info->left)->pc).id);
-                	printf("filho dir info: %d\n",((info->right)->pc).id);
-                	fpause2();   
-}*/
-
-//MAQUINA buscaID(ARV_BIN_ENC, int); // id
-//void buscaIDprint(ARV_BIN_ENC, int);
-
-//void remocaoPorCopia(ARV_BIN_ENC *, int);
-//void balancearArv(ARV_BIN_ENC *);
-
 void InOrdem(ARV_BIN_ENC arv) {
     if (arv) {
         InOrdem(left(arv)); 
